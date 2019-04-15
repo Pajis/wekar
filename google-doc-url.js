0ba9099bdcc326c3f4dba0d1769aebcd2cc2cff7
Pajis <PavelVyvlecka@seznam.cz>
2019-04-15 19:14:04 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yqmI-uF8t3paE6oclPjwCt6_1KwreCLeXvNLC8U1dAU/edit#gid=0';
